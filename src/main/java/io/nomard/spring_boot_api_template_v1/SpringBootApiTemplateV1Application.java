04edb4a04f06ada048e377c22c5674887c5261c2
Jonathan Mark Mwigo <mwigojm@gmail.com>
2024-03-29 12:00:02 +0300

    Initial commit

---
+++ src/main/java/io/nomard/spring_boot_api_template_v1/SpringBootApiTemplateV1Application.java
@@ -0,0 +1,13 @@
+package io.nomard.spring_boot_api_template_v1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootApiTemplateV1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootApiTemplateV1Application.class, args);
+    }
+
+}
